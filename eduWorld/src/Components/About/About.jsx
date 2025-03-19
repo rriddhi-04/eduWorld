d6b3e1af5c093e7c5ae42a3555c832cf10972a2a
rriddhi-04 <riddhimabhowmick@gmail.com>
2025-03-19 22:22:20 +0530

    about added

---
+++ eduWorld/src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className='about-left'>
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default About
