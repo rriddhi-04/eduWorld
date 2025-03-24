90cd8dfba415b77e129432192401cd8d62a38c7b
rriddhi-04 <riddhimabhowmick@gmail.com>
2025-03-24 21:02:42 +0530

    added testimonials and campus

--- eduWorld/src/App.jsx
+++ eduWorld/src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
 
 
 const App = () => {
@@ -15,6 +17,10 @@ const App = () => {
         <Title subTitle='Our Program' title='What we offer'/>
         <Programs/>
         <About/>
+        <Title subTitle='Gallery' title='Campus photo'/>
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What students says'/>
+        <Testimonials/>
       </div>
     </div>
   )
